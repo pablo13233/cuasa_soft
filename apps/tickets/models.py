db0a3414048bd4360f7726ad986c85b910c852a7
Pablo Acosta <acosta13233@hotmail.com>
2022-05-06 16:13:52 -0600

    modificaicones

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+def format_time_spent(time_spent):
+    if time_spent:
+        time_spent = "{0:02d}h:{1:02d}m".format(
+            time_spent.seconds // 3600,
+            time_spent.seconds % 3600 // 60
+        )
+    else:
+        time_spent = ""
+    return time_spent
+
+class Cola(models.Model):
+    titulo = models.CharField(max_length=100)
+    
