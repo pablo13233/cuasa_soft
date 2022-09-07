076c386fd323b081138019bcbb404c03b405a825
Pablo Acosta <acosta13233@hotmail.com>
2022-09-07 16:18:43 -0600

    asignaciones
    modificaciones a el modulo de asignaciones

--- apps/asignaciones/models.py
+++ apps/asignaciones/models.py
@@ -13,33 +13,19 @@ from django.forms import model_to_dict
 #def get_user_notes_unassinged_folder(instance, filename):
 #     return '{0}/{1}/{2}/{3}/{4}'.format('notas_desasignacion', instance.user_id.username, instance.created_at.year, ins tance.created_at.month , filename)
 
-class control_Asignaciones(models.Model):
-    id = models.AutoField(primary_key=True) 
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='asignado_a', null=False, blank=False)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creado_por')
-    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
-    update_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='actualizado_por')
-
-    def __str__(self):
-        return self.id
+class estado_asignacion(models.TextChoices):
+    ASIGNADO = 'ASIGNADO'
+    DESCARGO = 'DESCARGO'
     
-    def toJSON(self):
-        item = model_to_dict(self)
-        item['created_by'] = {'id': self.created_by.id, 'username': self.created_by.username}
-        item['assigned_by'] = {'id': self.assigned_by.id, 'username': self.assigned_by.username}
-        item['usuario'] = {'id': self.usuario.id,'username': self.usuario.username}
-        item['created_at'] = self.created_at.strftime("%Y-%m-%d")
-        item['updated_at'] = self.updated_at.strftime("%Y-%m-%d")
-        return item
 
 class historial_asignaciones(models.Model):
     id = models.AutoField(primary_key=True)
-    control_id = models.ForeignKey(control_Asignaciones, on_delete=models.CASCADE, null=False, blank=False)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='asignado_a', null=False, blank=False)
     inventario_item = models.ForeignKey(Inventario_Item, on_delete=models.CASCADE, null=False, blank=False )
-    assigned_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    status = models.CharField(max_length=25, choices=estado_asignacion.choices, default=estado_asignacion.ASIGNADO)
+    assigned_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     assigned_by = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name="asigna")
-    updated_at = models.DateTimeField(auto_now=True, null=False, blank=False)
+    updated_date = models.DateTimeField(auto_now=True, null=False, blank=False)
     update_by = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name="actualiza")
     observaciones = models.TextField(max_length=500, default="", blank=True, null=True)
 
@@ -49,8 +35,9 @@ class historial_asignaciones(models.Model):
     def  toJSON(self):
         item = model_to_dict(self)
         item['inventario_item']= {'id': self.inventario_item.id, 'correlativo': self.inventario_item.correlativo}
-        item['assigned_at'] = self.assigned_at.strftime("%Y-%m-%d")
+        item['usuario'] = {'id': self.usuario.id,'username': self.usuario.username}
+        item['assigned_date'] = self.assigned_at.strftime("%Y-%m-%d")
         item['assigned_by'] = {'id': self.assigned_by.id, 'username': self.assigned_by.username}
-        item['updated_at'] = self.updated_at.strftime("%Y-%m-%d")
+        item['updated_date'] = self.updated_at.strftime("%Y-%m-%d")
         item['update_by'] = {'id': self.update_by.id, 'username': self.update_by.username}
         return item
--- apps/asignaciones/views.py
+++ apps/asignaciones/views.py
@@ -19,26 +19,51 @@ def asignacionViews(request):
             #select
             action = request.POST['action']
             id_user = request.user_id
+            updated_time = datetime.now()
+            equipo = request.POST['item_id']
+            inv = Inventario_Item.objects.get(pk=equipo)
             if action == 'buscardatos': 
                 for i in historial_asignaciones.objects.all().order_by('-id'):
                     data.append(i.toJSON())
             #created
-            elif action == 'crear':
-                control_act=[]
-                for i in control_Asignaciones.objects.filter(usuario=request.POST['usr_control']):
-                    control_act.append(i.toJSON())
+            elif (action == 'crear') and (inv.status == 2):
                 
-                data = {'tipo_accion': 'crear', 'corrector': True}
+                asg = historial_asignaciones()
+                if int(request.POST['usuario_asignar']>0):
+                    asg.usuario = User.objects.get(pk=request.POST['usuario_asignar'])
+                if int(request.POST['item_id']>0):
+                    asg.inventario_item = Inventario_Item.objects.get(pk=equipo)
+                asg.assigned_by = User.objects.get(pk=id_user)
+                asg.update_by = User.objects.get(pk=id_user)
+                asg.observaciones = request.POST.get('observaciones')
+                asg.save()
+                #-------------------------------------
+                inv.status = 1
+                inv.updated_at = updated_time
+                inv.save()
+
+                data = {'tipo_accion': 'crear', 'correcto': True}
             #editar
-            elif action == 'editar':    
-                data = {'tipo_accion': 'editar', 'corrector': True}
+            elif action == 'editar':
+                
+                inv.status = 1
+                inv.updated_at = updated_time
+                inv.save()
+                #-------------------------------------
+                asg = historial_asignaciones.objects.get()
+                asg.estado = 'DESCARGO'
+                asg.updated_date = updated_time
+                asg.updated_by = User.objects.get(pk=id_user)
+                asg.save()
+
+                data = {'tipo_accion': 'editar', 'correcto': True}
             else:
                 data['error'] = 'Ha ocurrido un error.'
         except Exception as e:
             print(str(e))
             print(action)
             data['error'] = str(e)
-            data = {'tipo_accion': 'error', 'corrector': True}
+            data = {'tipo_accion': 'error', 'correcto': True}
         return JsonResponse(data, safe=False)
     elif request.method == 'GET': 
         invent = Inventario_Item.objects.all()
